eea529e5cd408bea986e751b40d3d46c51faf94c
christianonoh <onohchibyk@gmail.com>
2023-03-07 03:22:33 +0100

    Fix linter errors

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const openMenu = document.querySelector('.open-menu');
 const closeMenu = document.querySelector('.close-menu');
 const navLinks = document.querySelectorAll('.navbar-links');
 
-openMenu.addEventListener('click', function(){
-    menuItems.classList.add('display-mobile-menu');
+openMenu.addEventListener('click', () => {
+  menuItems.classList.add('display-mobile-menu');
 });
-closeMenu.addEventListener('click', function(){
-    menuItems.classList.remove('display-mobile-menu');
+closeMenu.addEventListener('click', () => {
+  menuItems.classList.remove('display-mobile-menu');
 });
 
-navLinks.forEach(element => {
-    element.addEventListener('click', function(){
-        menuItems.classList.remove('display-mobile-menu');
-    })
+navLinks.forEach((element) => {
+  element.addEventListener('click', () => {
+    menuItems.classList.remove('display-mobile-menu');
+  });
 });
