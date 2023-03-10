74d4aa6921e3e0be8108a91a49eeb7f94e63cf07
christianonoh <onohchibyk@gmail.com>
2023-03-10 16:26:15 +0100

    Fix linter errors

--- script.js
+++ script.js
@@ -235,10 +235,11 @@ function validateEmail(input, requiredMsg, invalidMsg, invalidCase) {
   }
   // validate email format //
   const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const lowerCaseChecker = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
   const email = input.value.trim();
   if (!emailRegex.test(email)) {
     return showError(input, invalidMsg);
-  } if (email.toLowerCase !== email) {
+  } if (!lowerCaseChecker.test(email)) {
     return showError(input, invalidCase);
   }
   return true;
